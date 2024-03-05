0c885e1e1c79bcf7bb15baaa7b889b615cfe4a08
taimoor1725 <159219973+taimoor1725@users.noreply.github.com>
2024-03-05 10:05:39 +0500

    Add files via upload

---
+++ min.js
@@ -0,0 +1,14 @@
+//camelCase
+//snake_case
+//PascalCaase
+///////////string////////////////////
+var userName = "M Taimoor"; //e.g. camelCase
+var user_name = "faiz_bahoo"; //e.g. snalle_case
+var UserName = "TalhaBahoo"; //e.g.  PascalCase
+/////////////////Bollean///////////////////
+var isUserExist = true;
+var is_user_exist = false;
+var IsUserExist = 1;
+// = is assignment operator
+console.log(userName, user_name, UserName);
+console.log(isUserExist, is_user_exist, IsUserExist);
---
+++ min.ts
@@ -0,0 +1,19 @@
+//camelCase
+//snake_case
+//PascalCaase
+///////////string////////////////////
+
+let userName = "M Taimoor" //e.g. camelCase
+let user_name = "faiz_bahoo" //e.g. snalle_case
+let UserName = "TalhaBahoo"  //e.g.  PascalCase
+
+/////////////////Bollean///////////////////
+
+let isUserExist = true;
+let is_user_exist = false;
+let IsUserExist = 1;
+
+// = is assignment operator
+
+console.log(userName, user_name, UserName)
+console.log(isUserExist, is_user_exist, IsUserExist)
